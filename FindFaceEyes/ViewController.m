1f5b275160482cf07366dd6aacdd2fb8b3e12079
chksong <chksong@qq.com>
2015-08-17 20:12:19 +0800

    Initial Commit

---
+++ FindFaceEyes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FindFaceEyes
+//
+//  Created by chksong on 15/8/17.
+//  Copyright (c) 2015年 chksong company. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
